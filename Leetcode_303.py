b0d83828b48e27c243901a0b2240d707e37e9b31
david <truongdung0502@gmail.com>
2025-11-01 00:08:07 -0400

    dung pre fix array

--- Leetcode_303.py
+++ Leetcode_303.py
@@ -1,10 +1,15 @@
-class NumArray:
 
+class NumArray:
     def __init__(self, nums: List[int]):
-        print('Constructor')
+        prefixArr = [0]*(len(nums)+1)
+        prefixSum = 0
+        for i in range(len(nums)):
+            prefixSum += nums[i]
+            prefixArr[i+1] = prefixSum
+        self.prefixArr = prefixArr
 
     def sumRange(self, left: int, right: int) -> int:
-        print('Sum Range')
+        return self.prefixArr[right+1] - self.prefixArr[left]
        
 obj = NumArray([-2,0,3,-5,2,1])
-obj.sumRange(1,2)
+obj.sumRange(0,2)
