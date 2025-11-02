9bb29e95adc7f727f8a4961d8fa0e72f5fd2f9d7
david <truongdung0502@gmail.com>
2025-11-02 00:58:34 -0400

    3^n

--- Leetcode_53.py
+++ Leetcode_53.py
@@ -1,9 +1,14 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        print('Max Sum Array')
-        return 0
+        n = len(nums)
+        max_val = float('-inf')
+        for i in range(n):
+            current_sum = 0
+            for j in range(1, n):
+                current_sum = current_sum + nums[j]
+                max_val = max(max_val, current_sum)
+        return max_val
     
 sol = Solution()
-
-sol.maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
+print(sol.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
 
