959ba701c70478ea73fd36463e5e4dcdcee8db92
David Truong <david.truong@captiveaire.com>
2025-11-04 17:04:40 -0500

    lc

--- Leetcode/Leetcode_26.py
+++ Leetcode/Leetcode_26.py
@@ -1,2 +1,13 @@
 class Solution:
-    def __init__(self):
+    def removeDuplicates(self, nums: list[int]) -> int:
+        if not nums:
+            return 0
+        
+        write_index = 1
+        
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[write_index] = nums[i]
+                write_index += 1
+        
+        return write_index
