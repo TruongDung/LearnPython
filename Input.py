161c7baed727cb23af9cc267283d2f4f93edb33a
david <truongdung0502@gmail.com>
2025-11-04 22:26:56 -0500

    cm

--- Input.py
+++ Input.py
@@ -1,2 +1,15 @@
-name = input("name: ")
-print(name)
+import math
+import heapq
+
+# name = input("name: ")
+# print(f'hello {name}')
+
+print (f"{math.pi:.2f}")
+pq = []
+
+heapq.heappush(pq, 'b')
+heapq.heappush(pq, 'c')
+heapq.heappush(pq, 'a')
+
+
+print(heapq.heappop(pq))
--- Leetcode_53.py
+++ Leetcode_53.py
@@ -11,4 +11,3 @@ class Solution:
     
 sol = Solution()
 print(sol.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
-
