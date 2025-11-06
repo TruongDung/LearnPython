15f20b5441af7caff37e52d6e9b807fe8b84cf2b
David Truong <david.truong@captiveaire.com>
2025-11-06 15:27:54 -0500

    trie

--- Leetcode/TestTrie.py
+++ Leetcode/TestTrie.py
@@ -1,36 +1,19 @@
+class MapSum:
 
-class TrieNode:
     def __init__(self):
-        self.children = {}
-        self.isEnd = False
+        self.hash_map = {}
 
-class Trie:
+    def insert(self, key: str, val: int) -> None:
+        self.hash_map[key] = val
+
+    def sum(self, prefix: str) -> int:
+        total = 0
+        for k in self.hash_map:
+            if len(prefix) <= len(k) and k[:len(prefix)] == prefix:
+                total += self.hash_map[k]
+        return total
     
-    def __init__(self):
-        self.root = TrieNode()
 
-    def insert(self, word: str) -> None:
-        currentNode = self.root
-        for char in word:
-            if char not in currentNode.children:
-                currentNode.children[char] = TrieNode()
-            currentNode = currentNode.children[char]
-        currentNode.isEnd = True
-    
-    def get_all_words(self):
-        words = []
-        self._dfs(self.root, "", words)
-        return words
-
-    def _dfs(self, node: TrieNode, prefix: str, words: list) -> None:
-        if node.isEnd:
-            words.append(prefix)
-        for char, childNode in node.children.items():
-            self._dfs(childNode, prefix + char, words)
-
-obj = Trie()
-obj.insert("cat")
-obj.insert("car")
-obj.insert("can")
-
-print(obj.get_all_words())  # Output: dict_keys(['c'])
+mapSum = MapSum()
+mapSum.insert("apple", 3)
+print(mapSum.sum("app"))
