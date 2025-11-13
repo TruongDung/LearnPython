3a7071456b1d6cfd8df11b63e98c45924e139226
david <truongdung0502@gmail.com>
2025-11-13 18:46:37 -0500

    new

--- Leetcode/Leetcode_2654.py
+++ Leetcode/Leetcode_2654.py
@@ -1,3 +1,23 @@
+import math
+from typing import List
+
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        return 0
+        n = len(nums)
+        ones = nums.count(1)
+        if ones > 0:
+            return n - ones
+
+        minOps = math.inf
+
+        for i, g in enumerate(nums):
+            for j in range(i + 1, n):
+                g = math.gcd(g, nums[j])
+                if g == 1:   # gcd(nums[i..j]:== 1
+                    minOps = min(minOps, j - i)
+                    break
+
+        return -1 if minOps == math.inf else minOps + n - 1
+    
+sol = Solution()
+sol.minOperations([2,6,3,4])
