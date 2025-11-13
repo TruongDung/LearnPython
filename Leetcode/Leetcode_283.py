fa1404bb28dfe87c2ae5a11e67fe22ab1c651ee0
David Truong <david.truong@captiveaire.com>
2025-11-13 15:52:18 -0500

    283

--- Leetcode/Leetcode_283.py
+++ Leetcode/Leetcode_283.py
@@ -6,7 +6,7 @@ class Solution:
         right = 0
         while(right< len(nums)):
             if(nums[right] !=0):
-                nums[left] , nums[right]= nums[right], nums[left]
+                nums[left], nums[right]= nums[right], nums[left]
                 left += 1
             right += 1
 
