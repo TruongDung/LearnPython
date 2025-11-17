5042bdae4b0bd86abce0e3fd28c12b735a4ce17e
David Truong <david.truong@captiveaire.com>
2025-11-17 16:47:51 -0500

    cm

--- Leetcode/Leetcode_713.py
+++ Leetcode/Leetcode_713.py
@@ -1,3 +1,19 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        # dp
+        if k <= 1:
+             return 0
+        
+        j = 0
+        count = 0
+        product = 1
+        for i in range(len(nums)):
+            product *= nums[i]
+            while product >= k:
+                product //= nums[j]
+                j += 1
+            count += i - j + 1
+
+        return count
+    
+sol = Solution()
+sol.numSubarrayProductLessThanK([1,1,1], 0)
