d2dfa6d72635cb70d06532fc8f4a1c9a503a3d32
David Truong <david.truong@captiveaire.com>
2025-11-19 09:53:26 -0500

    cm

--- Leetcode/Leetcode_905.py
+++ Leetcode/Leetcode_905.py
@@ -1,17 +1,11 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        i = 0
-        j = n - 1
-        for i in range(n):
-            if nums[i] % 2 !=0:
-                if nums[j] %2 == 0:
-                    # swap
-                    temp = nums[j]
-                    nums[j] = nums[i]
-                    nums[i] = temp
-                    j -= 1
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] % 2 == 0:
+                nums[j], nums[i]= nums[i], nums[j]
+                j += 1
         return nums
-                
+
 sol = Solution()
 sol.sortArrayByParity([3,1,2,4])
