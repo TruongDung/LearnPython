1fae08d622296e62135f9a33d8d0f02d52c8b6aa
David Truong <david.truong@captiveaire.com>
2025-11-21 16:04:07 -0500

    cm

---
+++ Leetcode/Leetcode_219.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        s = set()
+        for i in range(len(nums)):
+            if nums[i] in s: 
+                return True
+            s.add(nums[i])
+            if len(s) > k:
+                s.remove(nums[i-k])
+
+        return False
+    
+sol = Solution()
+sol.containsNearbyDuplicate([1,2,4,3,1], k = 3)
