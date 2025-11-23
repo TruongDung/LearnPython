8b2e928d3ed5191307e17a060c2548b16412f6bb
david <truongdung0502@gmail.com>
2025-11-22 23:32:05 -0500

    cm

--- Leetcode/Leetcode_242_1.py
+++ Leetcode/Leetcode_242_1.py
@@ -2,10 +2,20 @@ from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        for key, count in Counter(s).items():
-            print(key, count)
-        print(sorted(s))
-        return sorted(s) == sorted(t)
+    #     for key, count in Counter(s).items():
+    #         print(key, count)
+    #     print(sorted(s))
+    #     return sorted(s) == sorted(t)
+
+        freq = [0]*26
+        for index in range(len(s)):
+            freq[ord(s[index])-ord('a')] += 1
+            #freq[ord(t[index])-ord('a')] -= 1
+        for value in freq:
+            if value != 0:
+                return False
+        print(freq)
+        return True
     
 sol = Solution()
-print(sol.isAnagram('anagram', 'nagaram'))
+print(sol.isAnagram('abc', 'cba'))
