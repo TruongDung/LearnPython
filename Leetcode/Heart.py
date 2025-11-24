0a17391163fd00ca5e0932d3e9b26e1650330bc1
David Truong <david.truong@captiveaire.com>
2025-11-24 16:36:24 -0500

    cm

--- Leetcode/Heart.py
+++ Leetcode/Heart.py
@@ -37,3 +37,25 @@
 # ani.save("love.gif", writer='ffmpeg')
 # # Display the animation
 # plt.show()
+
+# temporary 205 
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        char_index_s = {}
+        char_index_t = {}
+
+        for i in range(len(s)):
+            if s[i] not in char_index_s:
+                char_index_s[s[i]] = i
+
+            if t[i] not in char_index_t:
+                char_index_t[t[i]] = i
+            
+            if char_index_s[s[i]] != char_index_t[t[i]]:
+                return False
+
+        return True
+    
+sol = Solution()
+sol.isIsomorphic('egg', 'add')
