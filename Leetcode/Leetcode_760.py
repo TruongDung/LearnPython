dfd9f2897a34d64ec967991c594a253ca0cc0c5f
David Truong <david.truong@captiveaire.com>
2025-11-25 12:34:53 -0500

    cm

--- Leetcode/Leetcode_760.py
+++ Leetcode/Leetcode_760.py
@@ -1,17 +1,15 @@
 from collections import defaultdict
 class Solution:
     def anagramMappings(self, nums1: List[int], nums2: List[int]) -> List[int]:
-       
-        hashmap = defaultdict(int) 
-        
-        for index, val in enumerate(nums2):
-            hashmap[val] = index
-        
-        
-        for index, val in enumerate(nums1):
-            nums1[index] = hashmap[val]
-        
-        return nums1
+        if len(nums1) != len(nums2):
+            return []
+        dict2 = defaultdict(int)
+        res = [0]*len(nums1)
+        for i, val in enumerate(nums2):
+            dict2[val] = i
+        for i,val in enumerate(nums1):
+            res[i] = dict2[val]
+        return res
     
 sol = Solution()
 sol.anagramMappings([12,28,46,32,50], [50,12,32,46,28])
