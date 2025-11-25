5a1bb0b05d0108b08e61e11e337336945b87277a
David Truong <david.truong@captiveaire.com>
2025-11-25 16:45:57 -0500

    cm

--- Leetcode/Leetcode_1394.py
+++ Leetcode/Leetcode_1394.py
@@ -1,17 +1,19 @@
 from collections import defaultdict
+from collections import Counter
 class Solution:
     def findLucky(self, arr: List[int]) -> int:
-        d = defaultdict(int)
-        for i in range(len(arr)):
-                d[arr[i]] += 1
+        # d = defaultdict(int)
+        # for i in range(len(arr)):
+        #         d[arr[i]] += 1
 
-        count = -1
+        # count = -1
 
-        for i in d:
-            if d[i] == i:
-                count = max(count, d[i])
-        return count
-    
+        # for i in d:
+        #     if d[i] == i:
+        #         count = max(count, d[i])
+        # return count
+        c = Counter(arr)
+        return max([num for num, freq in c.items() if num == freq], default=-1)
 
 sol = Solution()
 print(sol.findLucky([2,2,3,4]))
