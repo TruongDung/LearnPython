f95f2e320a5b9566e72fbcab78518e15c3d72f01
david <truongdung0502@gmail.com>
2025-12-03 23:07:25 -0500

    cm

--- Leetcode/Leetcode_220.py
+++ Leetcode/Leetcode_220.py
@@ -3,12 +3,12 @@ class Solution:
         if valueDiff < 0: return False # edge case 
         
         seen = {}
-        for i, x in enumerate(nums): 
-            bkt = x//(valueDiff+1)
-            if bkt in seen and i - seen[bkt][0] <= indexDiff: return True 
-            if bkt-1 in seen and i - seen[bkt-1][0] <= indexDiff and abs(x - seen[bkt-1][1]) <= valueDiff: return True 
-            if bkt+1 in seen and i - seen[bkt+1][0] <= indexDiff and abs(x - seen[bkt+1][1]) <= valueDiff: return True 
-            seen[bkt] = (i, x) 
+        for i, num in enumerate(nums): 
+            valueCheck = num//(valueDiff+1)
+            if valueCheck in seen and i - seen[valueCheck][0] <= indexDiff: return True 
+            if valueCheck-1 in seen and i - seen[valueCheck-1][0] <= indexDiff and abs(num - seen[valueCheck-1][1]) <= valueDiff: return True 
+            if valueCheck+1 in seen and i - seen[valueCheck+1][0] <= indexDiff and abs(num - seen[valueCheck+1][1]) <= valueDiff: return True 
+            seen[valueCheck] = (i, num) 
         return False 
     
 sol = Solution()
