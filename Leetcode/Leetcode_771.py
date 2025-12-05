106237ec83ed97fe88b8de65fea87c1a56f22e1f
david <truongdung0502@gmail.com>
2025-12-05 10:50:14 -0500

    cm

--- Leetcode/Leetcode_771.py
+++ Leetcode/Leetcode_771.py
@@ -1,18 +1,14 @@
 from collections import defaultdict
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        s = set()
         count = 0
-        # d = defaultdict(int)
-        # for i, val in enumerate(stones):
-        #     d[val] += 1
+        for i in jewels:
+            if i in stones:
+                count += 1
         
-        # for c in jewels:
-        #     count += d[c]
-
-
-
-
         return count
+
     
 sol = Solution()
 sol.numJewelsInStones("aA", "aAAbbbb" )
