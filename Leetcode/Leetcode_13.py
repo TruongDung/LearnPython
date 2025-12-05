2788a9b87c5c95dda331b511ada145434f9b4f84
david <truongdung0502@gmail.com>
2025-12-05 11:19:28 -0500

    cm1

--- Leetcode/Leetcode_13.py
+++ Leetcode/Leetcode_13.py
@@ -1,25 +1,21 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman = {
-        'I': 1, 'V': 5, 'X': 10,
-        'L': 50, 'C': 100, 'D': 500, 'M': 1000
-        }
-        
         total = 0
-        n = len(s)
-        for i in range(n):
-            current = roman[s[i]]
-            # Nếu chưa phải ký tự cuối và ký tự sau lớn hơn ký tự hiện tại
-            if i == n-1:
-                print('out')
-                
-            if i < n - 1 and roman[s[i + 1]] > current:
-                total -= current    # dạng trừ (IV, IX, XL, ...)
-            else:
-                total += current    # cộng bình thường
+        d = {'I': 1, 'V':5, 'X':10, 'L':50, 'C':100, 'D': 500, 'M': 1000}
+
+        for i in range(len(s)):
+            current = s[i]
+            if i + 1 < len(s) and d[s[i+1]]>d[current]: 
+                total = total - d[current]
+            else: 
+                total += d[current]
+
+        return total
+
+            
         
 
 
             
 sol = Solution()
-sol.romanToInt('MCMXCIV')
+print(sol.romanToInt('MCMXCIV'))
