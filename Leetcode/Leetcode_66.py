2e53a608ac84373858257e1f09002992fdff6ab5
David Truong <david.truong@captiveaire.com>
2025-12-12 16:51:30 -0500

    cm

--- Leetcode/Leetcode_66.py
+++ Leetcode/Leetcode_66.py
@@ -1,5 +1,9 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        memory = 1
+
+        for i in range(len(digits)-1, -1, -1): print(i)
+
         return []
     
 sol = Solution()
