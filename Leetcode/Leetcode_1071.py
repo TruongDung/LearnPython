1862b8f0d1a534ce819a00855c953e79e75ddbb8
David Truong <david.truong@captiveaire.com>
2025-12-18 16:38:56 -0500

    cm

--- Leetcode/Leetcode_1071.py
+++ Leetcode/Leetcode_1071.py
@@ -5,15 +5,16 @@ class Solution:
             return ""
         # If strings are equal than return the substring from 0 to gcd of size(str1), size(str2)
         from math import gcd
-        print(self.gcd(18, 48))
+        #a1 = gcd(len(str1), len(str2))
         return str1[:gcd(len(str1), len(str2))]
 
-    def gcd(self, a, b):
-        while b!=0:
-            a, b = b, a%b
-        return a
+    # def gcd(self, a, b):
+    #     while b!=0:
+    #         a, b = b, a%b
+    #     return a
     
 
 sol = Solution()
 #print(sol.gcdOfStrings("ABCABC", "ABC"))
-print(sol.gcdOfStrings("ABABAB", "ABAB"))
+#print(sol.gcdOfStrings("ABABAB", "ABAB"))
+print(sol.gcdOfStrings("AA", "A"))
