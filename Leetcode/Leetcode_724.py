dc09ab7875f8cd755fe2e6a95402b4075fe9e250
david <truongdung0502@gmail.com>
2025-12-22 23:19:45 -0500

    cm

--- Leetcode/Leetcode_724.py
+++ Leetcode/Leetcode_724.py
@@ -2,13 +2,25 @@ from typing import List
 
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        s = sum(nums)
-        sumleft = 0
-        for i in range(len(nums)):
-            if sumleft == (s - nums[i]) / 2:
+        n = len(nums)
+        prefix_sum_left = [0] * n
+        prefix_sum_right = [0] * n
+
+        sum = 0
+        for i in range(n):
+            sum += nums[i]
+            prefix_sum_left[i] = sum
+        
+        sum = 0
+        for i in range(n-1, -1, -1):
+            sum += nums[i]
+            prefix_sum_right[i] = sum
+        
+        for i in range(n):
+            if prefix_sum_left[i] == prefix_sum_right[i]: 
                 return i
-            sumleft += nums[i]
+
         return -1
     
 sol = Solution()
-print(sol.pivotIndex([1,-1,4]))
+print(sol.pivotIndex([1,7,3,6,5,6]))
