128c4f67b97a55967df5615aff2a0b84571a2268
david <truongdung0502@gmail.com>
2025-12-22 21:59:05 -0500

    cm

--- Leetcode/Leetcode_560.py
+++ Leetcode/Leetcode_560.py
@@ -1,5 +1,6 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+            self.prefixSum([1,2,3,4])
             sub_num = {0:1}
             total = count = 0
 
@@ -12,6 +13,15 @@ class Solution:
                 sub_num[total] = 1 + sub_num.get(total, 0)
             
             return count
+    
+    def prefixSum(self, nums):
+        prefix = [0]*len(nums)
+        prefix[0] = nums[0]
+        for i in range(1,len(nums)):
+            prefix[i] = prefix[i-1] + nums[i]
+             
+
+         
 
 sol = Solution()
 
