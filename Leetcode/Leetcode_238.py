74142be1d1284f3c96654fdc25a26ae2246e5b75
David Truong <david.truong@captiveaire.com>
2026-01-07 11:18:48 -0500

    cm

--- Leetcode/Leetcode_238.py
+++ Leetcode/Leetcode_238.py
@@ -1,28 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        left_product = [0]*len(nums)
-        right_product = [0]*len(nums)
-        product = 1
-        for i in range(len(nums)):
-            product *= nums[i]
-            left_product[i] = product
-       
-        product = 1
-        for i in range(len(nums)-1, -1, -1):
-            product *= nums[i]
-            right_product[i] = product
-
-
-        res = [0] * len(nums)
         n = len(nums)
-        for i in range(len(nums)):
-            if i == 0:
-                res[i] = right_product[i+1]
-            elif i == n-1:
-                res[i] = left_product[i-1]
-            else:
-                res[i] = left_product[i-1] * right_product[i+1]
+        res = [1] * n
 
+        product = 1
+        for i in range(1, n):
+            res[i] = nums[i-1] * res[i-1]
+
+        product = 1
+        for i in range(n - 1, -1, -1):
+            res[i] = res[i] * product
+            product =product * nums[i]
 
         return res
     
