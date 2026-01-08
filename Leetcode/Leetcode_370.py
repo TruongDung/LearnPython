f0ab64e77276683cebb6692adb0e020fb256c80e
david <truongdung0502@gmail.com>
2026-01-07 23:23:17 -0500

    cm

---
+++ Leetcode/Leetcode_370.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getModifiedArray(self, length: int, updates: List[List[int]]) -> int:
+        diff = [0] * length
+
+        for 
+
+        return res
+    
+sol = Solution()
+sol.getModifiedArray(5, [[1,3,2],[2,4,3],[0,2,-2]])
+#sol.getModifiedArray(1,[])
