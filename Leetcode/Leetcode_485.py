3cdc878ac6231d38c8b0f40df64751d6f7fb383f
david <truongdung0502@gmail.com>
2026-01-09 00:04:30 -0500

    cm

--- Leetcode/Leetcode_485.py
+++ Leetcode/Leetcode_485.py
@@ -1,4 +1,14 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-            
-        return 0
+        i = 0
+        max_length = 0
+
+        for j in range(len(nums)):
+            if nums[j] == 0: 
+                i = j + 1
+            max_length = max(max_length, j - i + 1)
+
+        return max_length
+    
+sol = Solution()
+print(sol.findMaxConsecutiveOnes([1,1,0,1,1,1]))
