0626a4ffd7a6f1f7fe152a123f9ecd8092ee207a
david <truongdung0502@gmail.com>
2026-01-17 09:03:41 -0500

    cm

--- Leetcode/Leetcode_941.py
+++ Leetcode/Leetcode_941.py
@@ -1,21 +1,20 @@
 class Solution:
-    def validMountainArray(self, a: List[int]) -> bool:
-        n = len(a)
-        if n < 3: return False 
+    def validMountainArray(self, arr: List[int]) -> bool:
+        if len(arr)<3: return False
+        i = 0 
+        j = len(arr) - 1
 
-        i, j = 0, n-1
-
-        
-        while i+1 < n and a[i] < a[i+1]:
+        while i < len(arr) and arr[i]< arr[i+1]:
             i += 1
 
-       
-        while j-1 >= 0 and a[j] < a[j-1]:
+        while j>0 and arr[j-1]> arr[j]:
             j -= 1
 
-        
-        return 0 < i == j < n-1
+        if i == j: return True
+
+        return False
     
 
 sol = Solution()
-print(sol.validMountainArray([0,2,3,4,5,2,1,0]))
+#print(sol.validMountainArray([0,2,3,4,5,2,1,0]))
+print(sol.validMountainArray([0,3,2,1]));
