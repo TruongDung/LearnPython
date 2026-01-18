ae9e383137b9a2fe357d610c373be09d78b26c6c
david <truongdung0502@gmail.com>
2026-01-17 23:57:24 -0500

    cm

--- Leetcode/Leetcode_713.py
+++ Leetcode/Leetcode_713.py
@@ -2,18 +2,16 @@ class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
         if k <= 1:
              return 0
-        
-        j = 0
+        i = 0
         count = 0
         product = 1
-        for i in range(len(nums)):
-            product *= nums[i]
-            while product >= k:
-                product //= nums[j]
-                j += 1
-            count += i - j + 1
-
+        for j in range(len(nums)):
+            product *= nums[j]
+            while product > k:
+                product //= nums[i]
+                i += 1
+            count = max(count, j - i + 1)
         return count
     
 sol = Solution()
-print(sol.numSubarrayProductLessThanK([1,1,1], 0))
+print(sol.numSubarrayProductLessThanK([10, 5, 2, 6], 100))
