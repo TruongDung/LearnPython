23cf4f39d633fc6b24822cbf62aa52d5f4e14ab4
David Truong <david.truong@captiveaire.com>
2026-01-19 16:34:05 -0500

    c

--- Leetcode/Leetcode_28.py
+++ Leetcode/Leetcode_28.py
@@ -1,13 +1,13 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        n, m = len(haystack), len(needle)
-        if m == 0:
-            return 0  # theo yêu cầu: nếu needle rỗng trả về 0
-        
-        for i in range(n - m + 1):
-            if haystack[i:i+m] == needle:
+        m = len(haystack)
+        n = len(needle)
+
+        for i in range(m-n+1):
+            if haystack[i:i+n] == needle:
                 return i
         return -1
     
 sol = Solution()
-print(sol.strStr("sqadbutsad", "sad"))
+#print(sol.strStr("sadbutsad", "sad"))
+print(sol.strStr("aaaa", "aaaa"))
