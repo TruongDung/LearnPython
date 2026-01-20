49bf783dd1960137725e56af7ad792294d3f2e80
david <truongdung0502@gmail.com>
2026-01-20 00:22:36 -0500

    cm

---
+++ Leetcode/Leetcode_3314.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            b = x | (x + a)
+            print(a1)
+        return []
+    
+sol = Solution()
+print(sol.minBitwiseArray([2,3,5,7]))
