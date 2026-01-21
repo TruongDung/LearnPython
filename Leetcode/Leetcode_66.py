10d3346a3b75ef134a1f98b3d81c57f9e7fb3c70
david <truongdung0502@gmail.com>
2026-01-21 00:30:50 -0500

    cm

--- Leetcode/Leetcode_66.py
+++ Leetcode/Leetcode_66.py
@@ -4,13 +4,12 @@ class Solution:
 
         for i in range(len(digits)-1, -1, -1):
             memory += digits[i]
-            digits[i] = memory % 10
-            memory //= 10
-
-        if memory: return [1]+digits
-
+            digits[i] = memory%10
+            memory = memory // 10
+            
+        if memory == 1: return [1] + digits
+        
         return digits
-    
 sol = Solution()
 print(sol.plusOne([1,2,3]))
 print(sol.plusOne([9,9,9]))
