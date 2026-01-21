72769dcd63a72df3fc014aaa352d0353c260e421
David Truong <david.truong@captiveaire.com>
2026-01-21 14:15:11 -0500

    cm

--- Leetcode/Leetcode_20.py
+++ Leetcode/Leetcode_20.py
@@ -1,25 +1,8 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        pairs = {
-        ')': '(',
-        '}': '{',
-        ']': '['
-        }
-        if not s or s[0] in pairs:
-            return False
         
-        stack = []
+        return False
 
-        for c in s:
-            if not stack and c in pairs:
-                return False
-            if c in pairs and stack:
-                top = stack.pop()
-                if pairs[c] != top:
-                    return False
-            else:
-                stack.append(c)
-        return not stack
     
 sol = Solution()
 print(sol.isValid("{[]}"))
