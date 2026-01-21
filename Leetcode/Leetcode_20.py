c27c287a070a24a67f9fafd31a80549023bbabd0
David Truong <david.truong@captiveaire.com>
2026-01-21 16:20:00 -0500

    cm

--- Leetcode/Leetcode_20.py
+++ Leetcode/Leetcode_20.py
@@ -1,8 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        
-        return False
+        stack = []
+        for char in s:
+            if char is '(':
+                stack.append(')')
+            elif char is '{':
+                stack.append('}')
+            elif char is '[':
+                stack.append(']')
+            elif len(stack) == 0 or stack.pop() != char:
+                return False
+        return len(stack) == 0
+
 
     
 sol = Solution()
-print(sol.isValid("{[]}"))
+print(sol.isValid("([])"))
