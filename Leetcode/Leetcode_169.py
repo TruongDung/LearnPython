a8f1af61ada78a6d616682f9ed6efc316e40a45e
david <truongdung0502@gmail.com>
2026-01-21 21:11:50 -0500

    cm

--- Leetcode/Leetcode_169.py
+++ Leetcode/Leetcode_169.py
@@ -3,13 +3,18 @@ from collections import defaultdict
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        m = defaultdict(int)
+        majority = nums[0]
+        count = 1
 
-        for i in nums:
-            m[i] += 1
-            
-            if m[i] > len(nums)//2:
-                return i
-        return 0
+        for i in range(1,len(nums)):
+            if count == 0:
+                count = 1
+                majority = nums[i]
+            elif majority != nums[i]:
+                count -= 1
+            else:
+                count += 1
+        
+        return majority
 sol = Solution()
-print(sol.majorityElement([3,2,3]))
+print(sol.majorityElement([2,2,1,1,1,2,2]))
