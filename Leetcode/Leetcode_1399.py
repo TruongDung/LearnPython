e75e758375452fdf319c867b28bbbe39a2a78169
david <truongdung0502@gmail.com>
2026-01-23 00:33:54 -0500

    cm

--- Leetcode/Leetcode_1399.py
+++ Leetcode/Leetcode_1399.py
@@ -2,16 +2,21 @@ from collections import defaultdict
 
 class Solution:
     def countLargestGroup(self, n: int) -> int:
-        group = defaultdict(int)
+        m = defaultdict(int)
+        for i in range(1,n+1):
+            sum1 = self.sum_digit(i)
+            m[sum1] += 1
+        max_value = max(m.values())
+        return list(m.values()).count(max_value)
+    
+    def sum_digit(self, j: int) -> int:
+        sum = 0
+        while j > 0:
+            sum += j%10
+            j = j//10
+        return sum
 
-        for i in range(1, n+1):
-            a1 = list(map(int, str(i)))
-            print(type(a1))
-            key = sum(map(int, str(i)))   # f(i) = sum of digits
-            group[key] += 1
-        
-        m = max(group.values())
-        return list(group.values()).count(m)
+
     
 sol = Solution()
-print(sol.countLargestGroup(13))
+print(sol.countLargestGroup(2))
