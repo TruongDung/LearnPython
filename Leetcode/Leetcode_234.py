a6659ae2fab7995b3f2c6f0c1e6a7576a105176a
david <truongdung0502@gmail.com>
2026-02-11 22:28:16 -0500

    cm

---
+++ Leetcode/Leetcode_234.py
@@ -0,0 +1,58 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # pre = None
+        # cur = head
+        # a = head
+        # while cur:
+        #     next_node = cur.next
+        #     cur.next = pre
+        #     pre = cur
+        #     cur = next_node
+        
+        # if pre == a:
+        #     return True
+        
+        # return False
+
+        if not head or not head.next:
+            return True
+
+        # 1. Find middle
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # 2. Reverse second half
+        prev = None
+        while slow:
+            next_node = slow.next
+            slow.next = prev
+            prev = slow
+            slow = next_node
+
+        # 3. Compare
+        left = head
+        right = prev
+
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+
+        return True
+
+    
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+
+sol = Solution()
+print(sol.isPalindrome(head))
