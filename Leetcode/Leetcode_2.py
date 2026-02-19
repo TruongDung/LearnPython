d0b3ecb949ab3980e95963eec94983785913f93f
David Truong <david.truong@captiveaire.com>
2026-02-19 11:01:52 -0500

    cm

--- Leetcode/Leetcode_2.py
+++ Leetcode/Leetcode_2.py
@@ -1,4 +1,4 @@
-class Solution:
+﻿class Solution:
     def addTwoNumbers(self, l1, l2):
         dummy = ListNode(0)
         current = dummy
@@ -28,22 +28,23 @@ class ListNode:
         self.val = val
         self.next = next
 
-# l1 = 2 -> 4 -> 3
-l1 = ListNode(2)
-l1.next = ListNode(4)
-l1.next.next = ListNode(3)
+if __name__ == "__main__":
+    # l1 = 2 -> 4 -> 3
+    l1 = ListNode(2)
+    l1.next = ListNode(4)
+    l1.next.next = ListNode(3)
 
-# l2 = 5 -> 6 -> 4
-l2 = ListNode(2)
-l2.next = ListNode(5)
-l2.next.next = ListNode(4)
+    # l2 = 5 -> 6 -> 4
+    l2 = ListNode(2)
+    l2.next = ListNode(5)
+    l2.next.next = ListNode(4)
 
+    sol = Solution()
 
-sol = Solution()
-def print_list(head):
-    while head:
-        print(head.val, end=" -> ")
-        head = head.next
-    print("None")
+    def print_list(head):
+        while head:
+            print(head.val, end=" -> ")
+            head = head.next
+        print("None")
 
-print_list(sol.addTwoNumbers(l1, l2))
+    print_list(sol.addTwoNumbers(l1, l2))
---
+++ tests/test_leetcode_2.py
@@ -0,0 +1,36 @@
+﻿import pytest
+
+from Leetcode.Leetcode_2 import Solution, ListNode
+
+
+def build_list(values):
+    dummy = ListNode(0)
+    cur = dummy
+    for v in values:
+        cur.next = ListNode(v)
+        cur = cur.next
+    return dummy.next
+
+
+def to_list(node):
+    out = []
+    while node:
+        out.append(node.val)
+        node = node.next
+    return out
+
+
+@pytest.mark.parametrize(
+    "l1, l2, expected",
+    [
+        ([2, 4, 3], [5, 6, 4], [7, 0, 8]),
+        ([9, 9, 9], [1], [0, 0, 0, 1]),
+        ([0], [0], [0]),
+        ([1, 8], [0], [1, 8]),
+    ],
+)
+
+def test_add_two_numbers(l1, l2, expected):
+    sol = Solution()
+    head = sol.addTwoNumbers(build_list(l1), build_list(l2))
+    assert to_list(head) == expected
