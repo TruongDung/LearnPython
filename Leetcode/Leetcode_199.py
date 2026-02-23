4dc1262cf452567b9f4c0f5d59855151b2888f36
david <truongdung0502@gmail.com>
2026-02-22 22:36:30 -0500

    cm

--- Leetcode/Leetcode_199.py
+++ Leetcode/Leetcode_199.py
@@ -4,6 +4,41 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def build_tree(lst):
+    if not lst:
+        return None
+
+    root = TreeNode(lst[0])
+    queue = [root]
+    i = 1
+
+    while queue and i < len(lst):
+        node = queue.pop(0)
+
+        # left child
+        if i < len(lst) and lst[i] is not None:
+            node.left = TreeNode(lst[i])
+            queue.append(node.left)
+        i += 1
+
+        # right child
+        if i < len(lst) and lst[i] is not None:
+            node.right = TreeNode(lst[i])
+            queue.append(node.right)
+        i += 1
+
+    return root
+
+# Test
+root = build_tree([3, 9, 20, None, None, 15, 7])
+
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        
+        return []
