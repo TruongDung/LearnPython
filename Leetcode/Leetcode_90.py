3ec7ab4ce0006891f41ba448802752d290e58171
david <truongdung0502@gmail.com>
2026-04-14 22:58:29 -0400

    cm

--- Leetcode/Leetcode_90.py
+++ Leetcode/Leetcode_90.py
@@ -1,3 +1,19 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        return []
+        res = []
+        nums.sort()
+        def dfs(start, temp):
+            if start == len(nums):
+                res.append(list(temp))
+
+            for i in range(start, n):
+                if i > start and nums[i] == nums[i+1]:
+                    continue
+
+                temp.append(nums[i])
+                dfs(start+1, temp)
+                temp.pop()
+                
+            dfs(0,[])
+
+        return res
