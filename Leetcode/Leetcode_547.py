0103873898583ccd6bd17bcff65809c17cb31443
David Truong <david.truong@captiveaire.com>
2026-04-20 15:05:50 -0400

    cm

--- Leetcode/Leetcode_547.py
+++ Leetcode/Leetcode_547.py
@@ -1,3 +1,27 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        return 0
+        n = len(isConnected)
+        visited = [False for _ in range(n)]
+        count = 0
+        def dfs(curr):
+            visited[curr] = True
+            for next in range(n):
+                if isConnected[curr][next] == 1:
+                    if not visited[next]:
+                        dfs(next)
+        
+        for i in range(n):
+            if not visited[i]:
+                dfs(i)
+                count += 1
+        return count
+    
+
+isConnected = [
+    [1, 1, 0],
+    [1, 1, 0],
+    [0, 0, 1]
+]
+
+sol = Solution()
+result = sol.findCircleNum(isConnected)
