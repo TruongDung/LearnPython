533dae50d9fb507a0d07a33686c0cd09ddf84a43
David Truong <david.truong@captiveaire.com>
2026-04-29 16:43:59 -0400

    cm

--- Leetcode/Leetcode_1197.py
+++ Leetcode/Leetcode_1197.py
@@ -1,3 +1,6 @@
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
+        x, y = abs(x), abs(y)
+
+        directions = [ (-2,-1), (-1,-2), (1, -2), (2, -1), (-1, 2), (1, 2), (2, 1)]
         return 0
