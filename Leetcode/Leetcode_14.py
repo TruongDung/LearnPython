063e92943783623dd715e621de37a0a4f4b2ad9f
David Truong <david.truong@captiveaire.com>
2026-04-29 16:39:51 -0400

    cm

--- Leetcode/Leetcode_14.py
+++ Leetcode/Leetcode_14.py
@@ -1,6 +1,17 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        return ''
+        result = ""
+        for i in range(len(strs[0])):
+            same = True
+            for word in strs:
+                if i >= len(word)  or word[i] != strs[0][i]:
+                    same = False
+                    break
+            if same:
+                result += strs[0][i]
+            else:
+                break
+        
     
 sol = Solution()
 print(sol.longestCommonPrefix(["flower","flow","flight"]))
