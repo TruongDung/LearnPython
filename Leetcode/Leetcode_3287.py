8c4758e20734471ec83e790aa4458e391efb9e8b
David Truong <david.truong@captiveaire.com>
2026-05-07 08:17:39 -0400

    cm

--- Leetcode/Leetcode_3287.py
+++ Leetcode/Leetcode_3287.py
@@ -5,11 +5,11 @@ class Solution:
         
         The maximum value of a subsequence of length k is the sum of the k largest elements.
         """
-        if k > len(nums):
-            return 0  # or raise error, but assume k <= n
+        # if k > len(nums):
+        #     return 0  # or raise error, but assume k <= n
         
-        # Sort in descending order
-        nums.sort(reverse=True)
-        # Sum the first k elements
-        return sum(nums[:k])
+        # # Sort in descending order
+        # nums.sort(reverse=True)
+        # # Sum the first k elements
+        # return sum(nums[:k])
         
