464428cfe91f0ae745f03268bacc29ae1e9012ec
David Truong <david.truong@captiveaire.com>
2026-05-11 15:19:53 -0400

    cm

---
+++ Leetcode/Leetcode_71.py
@@ -0,0 +1,17 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        parts = path.split('/')
+        for i in parts:
+            if i == "." or i == '':
+                continue
+            elif i =='..':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(i)
+        return '/' + '/'.join(stack)
+    
+sol = Solution()
+#print(sol.simplifyPath("/home//foo/"))
+print(sol.simplifyPath("/home/user/Documents/../Pictures"))
