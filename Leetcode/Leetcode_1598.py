fd594dc9bc97fc59180bf5be36f625982405cdbc
david <truongdung0502@gmail.com>
2026-05-11 01:18:06 -0400

    cm

--- Leetcode/Leetcode_1598.py
+++ Leetcode/Leetcode_1598.py
@@ -3,4 +3,14 @@ from typing import List
 
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
-        return 0
+        deep = 0
+        for i in logs:
+            if i == "../":
+                if deep>0:
+                    deep -= 1
+            elif i == "./":
+                continue
+            else:
+                deep += 1
+
+        return deep
