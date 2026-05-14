3410b3e0a39457e022930b0401485de48a5891ed
david <truongdung0502@gmail.com>
2026-05-13 22:34:09 -0400

    cm

--- Leetcode/Leetcode_921.py
+++ Leetcode/Leetcode_921.py
@@ -2,6 +2,10 @@ class Solution:
     def minAddToMakeValid(self, s: str) -> int:
         count = 0
         stack = []
+        for i in s:
+            if stack and i == ")":
+                stack.pop()
+            stack.append(i)
         
         return count
 
