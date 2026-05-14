feafcab121657f3d7a624c2b8375d0d84229c1af
david <truongdung0502@gmail.com>
2026-05-13 23:22:20 -0400

    cm

---
+++ Leetcode/Leetcode_1670.py
@@ -0,0 +1,22 @@
+class FrontMiddleBackQueue:
+
+    def __init__(self):
+        
+
+    def pushFront(self, val: int) -> None:
+        
+
+    def pushMiddle(self, val: int) -> None:
+        
+
+    def pushBack(self, val: int) -> None:
+        
+
+    def popFront(self) -> int:
+        
+
+    def popMiddle(self) -> int:
+        
+
+    def popBack(self) -> int:
+        
