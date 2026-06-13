2feb14f4d229bce1b4dac081d3568a95e3f3d847
david <truongdung0502@gmail.com>
2026-06-12 23:35:57 -0400

    Add initial implementation of mapWordWeights method in Solution class

---
+++ Leetcode/Leetcode_3838.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
+        
+        return ''
+
+        
+
+sol = Solution()
+words = ["abcd","def","xyz"], weights = [5,3,12,14,1,2,3,2,10,6,6,9,7,8,7,10,8,9,6,9,9,8,3,7,7,2]
+print(sol.mapWordWeights(words, weights))  
