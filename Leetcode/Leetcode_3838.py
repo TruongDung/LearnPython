c5470edf4d9e3043585ebc29143e7de51ca5868c
david <truongdung0502@gmail.com>
2026-06-13 00:19:54 -0400

    cm

--- Leetcode/Leetcode_3838.py
+++ Leetcode/Leetcode_3838.py
@@ -3,11 +3,20 @@ from typing import List
 
 class Solution:
     def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
+
+
         
-        return ''
+        ans = []
+        for word in words:
+            s = 0
+            for c in word:
+                s += weights[ord(c) - ord("a")]
+            ans.append(chr(ord("z") - s % 26))
+        return "".join(ans)
 
         
 
 sol = Solution()
-words = ["abcd","def","xyz"], weights = [5,3,12,14,1,2,3,2,10,6,6,9,7,8,7,10,8,9,6,9,9,8,3,7,7,2]
+words = ["abcd","def","xyz"]
+weights = [5,3,12,14,1,2,3,2,10,6,6,9,7,8,7,10,8,9,6,9,9,8,3,7,7,2]
 print(sol.mapWordWeights(words, weights))  
