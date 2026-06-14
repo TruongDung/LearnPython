1e0aefcf0f6589665706a852e2b465d8b789396f
david <truongdung0502@gmail.com>
2026-06-14 10:13:59 -0400

    cm

--- Leetcode/Leetcode_2130.py
+++ Leetcode/Leetcode_2130.py
@@ -5,4 +5,19 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        
+        slow = head
+        fast = head
+        stack = []
+
+        while fast is not None and fast.next is not None:
+            stack.append(slow.val)
+            slow = slow.next
+            fast = fast.next.next
+
+        max_sum = float('-inf')
+
+        while slow is not None and stack:
+            max_sum = max(max_sum, stack.pop() + slow.val)
+            slow = slow.next
+
+        return max_sum
