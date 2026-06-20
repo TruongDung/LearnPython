dc627d2ccb1aaaf913989fcb552c3cf9cc24ed22
david <truongdung0502@gmail.com>
2026-06-20 00:58:09 -0400

    cm

---
+++ Leetcode/Leetcode_15.py
@@ -0,0 +1,6 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        
+
+sol = Solution()
+print(sol.threeSum([-1,0,1,2,-1,-4]))
--- Leetcode_208.py
+++ Leetcode_208.py
@@ -1,14 +1,9 @@
 class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.is_word = False
 
 class Trie:
 
     def __init__(self):
         self.root = TrieNode()
-
-        
 
     def insert(self, word: str) -> None:
         node = self.root
