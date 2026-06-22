f47b48fe542d86ebe391db04c63a5354ba3d96d6
david <truongdung0502@gmail.com>
2026-06-21 23:16:28 -0400

    Add initial implementation of MagicDictionary class with constructor and method stubs

---
+++ Leetcode/Leetcode_676.py
@@ -0,0 +1,13 @@
+class MagicDictionary:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        
+        
+
+    def buildDict(self, dictionary: List[str]) -> None:
+        
+
+    def search(self, searchWord: str) -> bool:
