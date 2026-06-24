f18e2766440aadfb4ad48ab28ffa98f692e6b72a
david <truongdung0502@gmail.com>
2026-06-24 00:17:21 -0400

    cm

--- Leetcode/Leetcode_676.py
+++ Leetcode/Leetcode_676.py
@@ -1,24 +1,43 @@
 from ast import List
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
 
 class MagicDictionary:
 
     def __init__(self):
-        #self.root = TrieNode()
-        self.dic = set()
+        self.root = TrieNode()
         
     def buildDict(self, dictionary: List[str]) -> None:
-        # node = self.root
-
         for word in dictionary:
-            self.dic.add(word)
+            node = self.root
 
+            for char in word:
+                if char not in node.children:
+                    node.children[char] = TrieNode()
+                node = node.children[char]
+            node.is_word = True
     def search(self, searchWord: str) -> bool:
-        for word in self.dic:
-            if word == searchWord:
-                return True
+        def dfs(node, index, modified):
+            if index == len(searchWord):
+                return modified and node.is_word
+            
+            char = searchWord[index]
+            if char in node.children:
+                if dfs(node.children[char], index + 1, modified):
+                    return True
+            
+            if not modified:
+                for child_char, child_node in node.children.items():
+                    if child_char != char:
+                        if dfs(child_node, index + 1, True):
+                            return True
+            
+            return False
         
-        return False
+        return dfs(self.root, 0, False)
     
 sol = MagicDictionary()
 sol.buildDict(["hello", "hallo", "leetcode"])
