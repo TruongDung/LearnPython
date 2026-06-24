a5faf9ee1dcd1d6228e44c9cce422f943bbb7f95
David Truong <david.truong@captiveaire.com>
2026-06-24 14:29:49 -0400

    cm

--- Leetcode/Leetcode_438.py
+++ Leetcode/Leetcode_438.py
@@ -3,20 +3,22 @@ class Solution:
         need = Counter(p)
         window = Counter()
 
+
         res = []
         left = 0
         k = len(p)
 
-        for right in range(lens(s)):
+        for right in range(len(s)):
             window[s[right]] += 1
 
-            if right - left + 1 > k:
+            if right - left + 1> k:
                 window[s[left]] -= 1
                 if window[s[left]] == 0:
-                    del window[s[left]]
+                    def window[s[left]]
                 left += 1
 
             if right - left + 1 == k and window == need:
                 res.append(left)
+
 
         return res
