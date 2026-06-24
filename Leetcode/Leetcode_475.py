24dbfbf4ffe8fc6fcd91b61dbb74429b930e9169
David Truong <david.truong@captiveaire.com>
2026-06-24 14:59:48 -0400

    cm

---
+++ Leetcode/Leetcode_475.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        heaters.sort()
+        hourse.sort()
+
+        res = 0
+        j = 0
+        n = len(heaters)
+
+        for h in houses:
+            while j + i < n and heaters[j+1] <= h:
+                j += 1
+
+                lest_dist = abs(n-heaters[j])
+                right_dist = float('inf')
+
+                if j + 1 < n:
+                    right_dist = abs(heaters[j+1] - h)
+
+                res = max(res, min(lest_dist, right_dist))
+        return res
