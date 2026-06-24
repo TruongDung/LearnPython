7fc44ef34ceff29a87b07b5191855419778f3227
David Truong <david.truong@captiveaire.com>
2026-06-24 14:06:45 -0400

    cm

---
+++ Leetcode/Leetcode_438.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        need = Counter(p)
+        window = Counter()
+
+        res = []
+        left = 0
+        k = len(p)
+
+        for right in range(lens(s)):
+            window[s[right]] += 1
+
+            if right - left + 1 > k:
+                window[s[left]] -= 1
+                if window[s[left]] == 0:
+                    del window[s[left]]
+                left += 1
+
+            if right - left + 1 == k and window == need:
+                res.append(left)
+
+        return res
