98ed0f2064980cd55abe9140e95ec0d195d3e3a4
david <truongdung0502@gmail.com>
2026-06-24 00:12:40 -0400

    cm

--- Leetcode/Leetcode_676.py
+++ Leetcode/Leetcode_676.py
@@ -1,13 +1,28 @@
+from ast import List
+
+
 class MagicDictionary:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
+        #self.root = TrieNode()
+        self.dic = set()
         
-        
-
     def buildDict(self, dictionary: List[str]) -> None:
-        
+        # node = self.root
+
+        for word in dictionary:
+            self.dic.add(word)
 
     def search(self, searchWord: str) -> bool:
+        for word in self.dic:
+            if word == searchWord:
+                return True
+        
+        return False
+    
+sol = MagicDictionary()
+sol.buildDict(["hello", "hallo", "leetcode"])
+print(sol.search("hello"))
+print(sol.search("hhllo"))
+print(sol.search("hell"))
+print(sol.search("leetcoded"))
