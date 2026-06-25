e8ef4e2690e3046c57c3cbdda81ecd24a6e2d21f
david <truongdung0502@gmail.com>
2026-06-25 00:54:54 -0400

    cm

---
+++ Leetcode/Leetcode_207.py
@@ -0,0 +1,35 @@
+from ast import List
+import collections
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+
+        for course, pre in prerequisites:
+            graph[pre].append(course)
+            indegree[course] += 1
+
+
+        queue = collections.deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+
+        count = 0
+
+        while queue:
+            size = len(queue)
+            for _ in range(size):
+                curr = queue.popleft()
+                count += 1
+
+                for neighbor in graph[curr]:
+                    indegree[neighbor] -= 1
+                    if indegree[neighbor] == 0:
+                        queue.append(neighbor)
+
+        return count == numCourses
+    
+    
