2659b1929ea510fc85e26981e82c0103fcfb393a
David Truong <david.truong@captiveaire.com>
2026-06-29 11:36:38 -0400

    feat(leetcode): add LeetCode 198 (House Robber) solution and visualization

--- leetcode-visualizer/server.js
+++ leetcode-visualizer/server.js
@@ -5877,6 +5877,12 @@ const SUPPORTED = {
     code: [
       "class Solution:",
       "    def rob(self, nums):",
+      "        if len(nums) == 0:",
+      "            return 0",
+      "        if len(nums) == 1:",
+      "            return nums[0]",
+      "        if len(nums) == 2:",
+      "            return max(nums[0], nums[1])",
       "        dp = [0] * len(nums)",
       "        dp[0] = nums[0]",
       "        dp[1] = max(nums[0], nums[1])",
