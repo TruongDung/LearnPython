b365d537971f21347b99ac5621d839721a24b07d
david <truongdung0502@gmail.com>
2026-07-12 00:35:04 -0400

    1331

--- Leetcode-sln/Leetcode_1331.py
+++ Leetcode-sln/Leetcode_1331.py
@@ -1,3 +1,19 @@
+from collections import defaultdict
+from collections import Counter
+
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        
+        num_to_rank = {}
+        count = 0
+        for num in sorted(arr):
+            if num not in num_to_rank:
+                count += 1
+            num_to_rank[num] = count
+            
+        res = []
+        for num in arr:
+            res.append(num_to_rank[num])
+        return res
+
+sol = Solution()
+print(sol.arrayRankTransform([40,40,10,20,30]))
