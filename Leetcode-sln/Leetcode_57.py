c692d6b3b4e91603f3d9df764fd3db4c5eba1eb9
David Truong <david.truong@captiveaire.com>
2026-08-06 16:48:52 -0400

    add leetcode 57

---
+++ Leetcode-sln/Leetcode_57.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        merged = []
+        for start, end in intervals:
+            a1 = intervals[start]
+        return 
+
+
+sol = Solution()
+sol.insert([[1,3],[6,9]], [2,5])
