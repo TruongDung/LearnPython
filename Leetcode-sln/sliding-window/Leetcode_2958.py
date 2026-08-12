8b1b6b1693dbed85cc5991f277f909abef70e05d
david <truongdung0502@gmail.com>
2026-08-11 22:08:12 -0400

    Implement maxSubarrayLength method for sliding window solution

--- Leetcode-sln/sliding-window/Leetcode_2958.py
+++ Leetcode-sln/sliding-window/Leetcode_2958.py
@@ -1,5 +1,18 @@
+from typing import List
+
+
 class Solution:
     def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+        left = 0
+        ans = 0
+        count = {} # {1:3}
+        for right in range(len(nums)):
+            #num[right] = 1
+            count[nums[right]] = count.get(nums[right], 0) + 1
+            while count[nums[right]] > k:
+                count[nums[left]] -= 1
+                left += 1
+            ans = max(ans, right - left + 1)
         return 0
 
 sol = Solution()
