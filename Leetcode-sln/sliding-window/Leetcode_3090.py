07427295848035c76c9bd93d3f58e4e854140f1b
david <truongdung0502@gmail.com>
2026-08-13 22:18:05 -0400

    feat: implement maximum length substring function for sliding window problem

---
+++ Leetcode-sln/sliding-window/Leetcode_3090.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maximumLengthSubstring(self, s: str) -> int:
+        left = 0
+        max_occ = 0
+        dic_s = {}
+        
+        for right in range(len(s)):
+            dic_s[s[right]] = dic_s.get(s[right], 0 ) +1
+            while dic_s[s[right]] > 2:
+                dic_s[s[left]] -= 1
+                left += 1
+
+            max_occ = max(max_occ, right - left + 1)
+
+        return max_occ
+
+sol = Solution()
+print(sol.maximumLengthSubstring("bcbbbcba"))
