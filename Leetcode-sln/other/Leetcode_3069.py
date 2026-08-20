fcf6e3647781fa0e5e73e83efcb7c7c16e75a857
david <truongdung0502@gmail.com>
2026-08-19 21:47:48 -0400

    Add 3069

---
+++ Leetcode-sln/other/Leetcode_3069.py
@@ -0,0 +1,10 @@
+class Solution:
+    def resultArray(self, nums: List[int]) -> List[int]:
+        ar1 = [nums[0]]
+        ar2 = [nums[1]]
+        for i in range(2, len(nums)):
+            if ar1[-1] > ar2[-1]:
+                ar1.append(nums[i])
+            else:
+                ar2.append(nums[i])
+        return ar1 + ar2
