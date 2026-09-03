454b759a0fb609e8edfdd6dc72373000c47a0998
david <truongdung0502@gmail.com>
2026-09-02 23:49:19 -0400

    feat: implement uniformArray function to check evenness in list

---
+++ Leetcode-sln/other/Leetcode_3876.py
@@ -0,0 +1,18 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        min_value = min(nums1)
+        is_even = True
+        for num in nums1:
+            if num%2 != 0:
+                is_even = False
+
+        if is_even:
+            return True
+
+        if min_value %2 != 0:
+            return True
+
+        return False
+
+sol = Solution()
+print(sol.uniformArray([2,3]))
