dccb93aa744712b722527791b7e123c233dbacc6
david <truongdung0502@gmail.com>
2026-09-26 00:24:34 -0400

    feat: refactor evaluate method to use lookup for knowledge mapping

--- Leetcode-sln/other/Leetcode_1807.py
+++ Leetcode-sln/other/Leetcode_1807.py
@@ -2,13 +2,33 @@ import re
 class Solution:
     def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
 
-        for a in knowledge:
-            if a[0] in s:
-                s = s.replace("(" + a[0] + ")", a[1])
+        # for a in knowledge:
+        #     if a[0] in s:
+        #         s = s.replace("(" + a[0] + ")", a[1])
             
-        s = re.sub(r"\([^)]*\)", "?", s)
+        # s = re.sub(r"\([^)]*\)", "?", s)
 
-        return s
+        # return s
+
+        lookup = dict(knowledge)
+
+        result = []
+        i = 0
+
+        while i < len(s):
+            if s[i] == "(":
+                j = s.find(")", i)
+
+                key = s[i + 1:j]
+
+                result.append(lookup.get(key, "?"))
+
+                i = j + 1
+            else:
+                result.append(s[i])
+                i += 1
+
+        return "".join(result)
 
 sol = Solution()
 print(sol.evaluate("(name)is(age)yearsold", [["name","bob"],["age","two"]]))
